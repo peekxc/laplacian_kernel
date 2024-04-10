03888715330908ab2976b82eb596fc0187c51f85
peekxc <matt.piekenbrock@gmail.com>
2024-04-10 09:24:10 -0400

    build chanegs to move towards C++ solution

--- laplacian.cu
+++ laplacian.cu
@@ -1,10 +1,7 @@
-#include <vector> 
-#include <cinttypes>
- 
+#include <cinttypes> 
 using index_t = int64_t;
-using std::vector;
-using vector2D = std::vector< std::vector< index_t > >;
 
+// Recursive binomial coefficient calculation - supports degenerate cases 
 // Baseline from: https://stackoverflow.com/questions/44718971/calculate-binomial-coffeficient-very-reliably
 // Requires O(min{k,n-k}), uses pascals triangle approach (+ degenerate cases)
 __device__ size_t binom(size_t n, size_t k) {
@@ -17,26 +14,8 @@ __device__ size_t binom(size_t n, size_t k) {
     (binom(n-1,k) * n)/(n-k);      //  path to k=n-1 is faster
 }
 
-// Table to cache low values of the binomial coefficient
-// template< typename value_t = index_t >
-// struct BinomialCoefficientTable {
-//   size_t pre_n = 0;
-//   size_t pre_k = 0; 
-//   vector< vector< value_t > > BT;
-
-//   // Safe, general binomial coefficient; uses cached table if possible 
-//   value_t operator()(const index_t n, const index_t k) const {
-//     if (n <= pre_n && k <= pre_k){ return BT[k][n]; } 				// runtime computed extension table
-//     return static_cast< value_t >(binom(n,std::min(k,n-k)));
-//   }
-//   // Fast, unsafe access to a precompute table
-//   constexpr auto at(index_t n, index_t k) const noexcept -> index_t {
-//     return BT[k][n];
-//   }
-
-// Precompute a *larger* table of binomial coefficients
-void precompute(const index_t n, const index_t k, index_t** BT){
-  // auto BT = std::vector< std::vector< value_t > >(k + 1, std::vector< value_t >(n + 1, 0));
+// Precomputes a table of binomial coefficients
+void precompute_BC(const index_t n, const index_t k, index_t** BT){
   for (index_t i = 0; i <= n; ++i) {
     BT[0][i] = 1;
     for (index_t j = 1; j < std::min(i, k + 1); ++j){
@@ -45,16 +24,14 @@ void precompute(const index_t n, const index_t k, index_t** BT){
     if (i <= k) { BT[i][i] = 1; };
   }
 }
-// }; // BinomialCoefficientTable
 
-__device__ index_t get_max(index_t top, index_t bottom, const index_t r, const index_t m, const vector2D& BT) noexcept {
+__device__ index_t get_max(index_t top, index_t bottom, const index_t r, const index_t m, const index_t** BT) noexcept {
   if (!(BT[m][bottom] <= r)) { return bottom; }
   index_t size = (top - bottom);
   while (size > 0){
     index_t step = size >> 1;
     index_t mid = top - step;
     if (BT[m][mid] <= r){
-    // if (!pred(mid)){
       top = mid - 1;
       size -= step + 1;
     } else {
@@ -64,7 +41,7 @@ __device__ index_t get_max(index_t top, index_t bottom, const index_t r, const i
   return top;
 }
 
-__device__ index_t get_max_vertex(const index_t r, const index_t m, const index_t n, const vector2D& BT) noexcept {
+__device__ index_t get_max_vertex(const index_t r, const index_t m, const index_t n, const index_t** BT) noexcept {
   index_t k_lb = m - 1;
   return 1 + get_max(n, k_lb, r, m, BT);
 }
@@ -86,7 +63,7 @@ __device__ void enum_boundary(const int n, const int simplex, const int dim, con
   }
 }
 
-__device__ void k_boundary(const index_t n, const index_t simplex, const index_t dim, const vector2D& BT, index_t* br) {
+__device__ void k_boundary(const index_t n, const index_t simplex, const index_t dim, const index_t** BT, index_t* br) {
   index_t idx_below = simplex;
   index_t idx_above = 0; 
   index_t j = n - 1;
@@ -98,17 +75,6 @@ __device__ void k_boundary(const index_t n, const index_t simplex, const index_t
     idx_below -= c;
     idx_above += BT[k][j];
     br[dim-k] = face_index;
-  }
-}
-
-void compute_deg_full(const int n, const int k, const vector2D& BT, std::vector< size_t >& deg){
-  for (size_t r = 0; r < BT[k][n]; ++r){
-    // enum_boundary(n, r, k - 1, BT, [&](int pr){ deg[pr] += 1; return true; });
-    index_t ps[3]; 
-    k_boundary(n, r, k - 1, BT, ps);
-    for(size_t i = 0; i < 3; ++i){
-      deg[ps[i]] += 1;
-    }
   }
 }
 
