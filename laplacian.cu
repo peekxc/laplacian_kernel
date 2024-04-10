e2e4e3c65010cdfa631c09be92361833b58218d0
peekxc <matt.piekenbrock@gmail.com>
2024-04-10 09:36:44 -0400

    update removing templates + vector

--- laplacian.cu
+++ laplacian.cu
@@ -46,23 +46,6 @@ __device__ index_t get_max_vertex(const index_t r, const index_t m, const index_
   return 1 + get_max(n, k_lb, r, m, BT);
 }
 
-// Enumerates the facets on the boundary of 'simplex'
-template< typename Lambda > 
-__device__ void enum_boundary(const int n, const int simplex, const int dim, const vector2D& BT, Lambda&& f) {
-  index_t idx_below = simplex;
-  index_t idx_above = 0; 
-  index_t j = n - 1;
-  bool cont_enum = true; 
-  for (index_t k = dim; k >= 0 && cont_enum; --k){
-    j = get_max_vertex(idx_below, k + 1, j, BT) - 1; // NOTE: Danger!
-    index_t c = BT[k+1][j];
-    index_t face_index = idx_above - c + idx_below;
-    idx_below -= c;
-    idx_above += BT[k][j];
-    cont_enum = f(face_index);
-  }
-}
-
 __device__ void k_boundary(const index_t n, const index_t simplex, const index_t dim, const index_t** BT, index_t* br) {
   index_t idx_below = simplex;
   index_t idx_above = 0; 
@@ -79,20 +62,19 @@ __device__ void k_boundary(const index_t n, const index_t simplex, const index_t
 }
 
 // Kernel for adding the boundary/coboundary of r 
-// k should == array size 
-__global__ void add_boundaries(const index_t k, const index_t n, const index_t N, const index_t* sgn_pattern, const vector2D& BT, const float* x, float *y)
+__global__ void add_boundaries(const index_t k, const index_t n, const index_t N, const index_t** BT, const float* x, float *y)
 {
   int tid = blockDim.x * blockIdx.x + threadIdx.x;
   if (tid < N){
     index_t ps[3]; 
     k_boundary(n, tid, k - 1, BT, &ps);
-    int cc = 0; 
-    for (int ii = 0; ii < k; ++ii){
-      for (int jj = ii + 1; jj < k; ++jj){
-        atomicAdd(y + ps[ii], sgn_pattern[cc] * x[ps[jj]]);
-        ++cc;
-      }
-    }
+    index_t i = ps[0], j = ps[1], q = ps[2];
+    atomicAdd(y + i, -x[j]);
+    atomicAdd(y + j, -x[j]);
+    atomicAdd(y + i, x[q]);
+    atomicAdd(y + q, x[i]);
+    atomicAdd(y + j, -x[q]);
+    atomicAdd(y + q, -x[j]);
   }  
 }
 
