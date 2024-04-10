c63aede47954c5e0ff80ceb6edbe6bf688c66a16
peekxc <matt.piekenbrock@gmail.com>
2024-04-10 09:36:54 -0400

    Modifying test

--- test.py
+++ test.py
@@ -4,5 +4,4 @@ import urllib.request
 laplacian_url = "https://raw.githubusercontent.com/peekxc/laplacian_kernel/main/laplacian.cu"
 f = urllib.request.urlopen(laplacian_url)
 LP_cu = f.read()
-
-wut = print(f"{LP_cu}")
+LP_cu.decode("utf-8").replace("\\n", "\n")
