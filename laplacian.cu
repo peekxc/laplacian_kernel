0933b5f732a84b355e08579e8197b02e5fb1139a
peekxc <matt.piekenbrock@gmail.com>
2024-04-11 10:05:48 -0400

    bug fix

--- laplacian.cu
+++ laplacian.cu
@@ -78,10 +78,10 @@ __global__ void add_boundaries(const index_t k, const index_t n, const index_t N
   }  
 }
 
-void compute_deg_full(const int n, const int k, const index_t* BT, index_t* deg){
+void compute_deg_full(const int n, const int k, const index_t** BT, index_t* deg){
   for (size_t r = 0; r < binom(n, k); ++r){
     index_t ps[4] = { 0, 0, 0, 0 }; 
-    k_boundary(n, r, k - 1, BT, (index_t*) ps);
+    k_boundary(n, r, k - 1, BT, (const index_t*) ps);
     deg[ps[0]] += 1;
     deg[ps[1]] += 1;
     deg[ps[2]] += 1;
